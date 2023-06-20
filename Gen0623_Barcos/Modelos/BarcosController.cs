5b5fab09ebf7d3cefad9796986174536895d2d4b
juanbufon13 <lopezjuanluis591@gmail.com>
2023-06-19 22:22:32 -0600

    barco

---
+++ Gen0623_Barcos/Modelos/BarcosController.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.NetworkInformation;
+
+namespace Gen0623_Barcos.Modelos
+{
+    public class BarcosController
+    {
+        public class barcoJL
+        {
+            [Key]            
+            [Required(ErrorMessage = "El Id es requerido")]
+            public int Id { get; set; }
+            [Required(ErrorMessage = "El Nombre es requerido")]
+            public string Nombre { get; set; }
+            [Required(ErrorMessage = "El tipo del barco es requerido")]
+            public string Tipo { get; set; }
+            public string CapacidadToneladas { get; set; }
+            public Estatus Status { get; set; }
+        }
+    }
+}
---
+++ Gen0623_Barcos/Modelos/Estatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Gen0623_Barcos.Modelos
+{
+    public class Estatus
+    {
+        public enum Estatus
+        {
+            Disponible = 0,
+            NoDisponible = 1,
+        }
+    }
+}
